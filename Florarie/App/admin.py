71676fb92893e6f67b8e0ea1c9a3b20d0ad60e1e
enculescu.bianca.h9n <enculescu.bianca.h9n@student.ucv.ro>
2025-03-12 21:04:29 +0200

    update

--- Florarie/App/admin.py
+++ Florarie/App/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Product
+from .models import Product, User
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'image_preview')
@@ -13,3 +13,4 @@ class ProductAdmin(admin.ModelAdmin):
     image_preview.short_description = "Image"
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(User)
--- Florarie/App/models.py
+++ Florarie/App/models.py
@@ -1,6 +1,23 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',  # Add related_name to avoid conflict
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions_set',  # Add related_name to avoid conflict
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
